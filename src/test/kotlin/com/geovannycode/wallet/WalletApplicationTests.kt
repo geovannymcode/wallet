b9c72eab5962f965351dd8383ae9e072757c5baf
Geovanny Mendoza <me@geovannycode.com>
2026-07-08 12:00:00 -0500

    fase-0: proyecto wallet, estructura por features y conexión a Supabase

---
+++ src/test/kotlin/com/geovannycode/wallet/WalletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.geovannycode.wallet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WalletApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
