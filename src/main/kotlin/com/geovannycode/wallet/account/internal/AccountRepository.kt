f5ace340a2f1d7689dca57d3f37302c4a1fe3803
Geovanny Mendoza <me@geovannycode.com>
2026-07-09 12:00:00 -0500

    fase-1: entidad Account, repositorio y migración inicial con Flyway

---
+++ src/main/kotlin/com/geovannycode/wallet/account/internal/AccountRepository.kt
@@ -0,0 +1,6 @@
+package com.geovannycode.wallet.account.internal
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface AccountRepository : JpaRepository<Account, UUID>
